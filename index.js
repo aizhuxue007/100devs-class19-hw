21c3cd458ed6372607c7b6c7177077c566f83479
aizhuxue007 <aizhuprogram@gmail.com>
2023-10-13 22:19:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/** THE JSWAY */
+
+/** Chapter 7 Arrays */
+
+// Musketeers
+// Write a program that:
+
+// Creates an array named musketeers containing values "Athos", "Porthos" and "Aramis".
+// Shows each array element using a for loop.
+// Adds the "D'Artagnan" value to the array.
+// Shows each array element using the forEach() method.
+// Remove poor Aramis.
+// Shows each array element using a for-of loop.
+
+
+// Sum of values
+// Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case).
+
+
+
+// Array maximum
+// Write a program that creates the following array, then calculates and shows the array's maximum value
+
+
+// List of words
+// Write a program that asks the user for a word until the user types "stop". The program then shows each of these words, except "stop".
